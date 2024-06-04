006589a825be34e0d2803301986fb44687e249ee
jradi mohamed <jradi93@gmail.com>
2024-06-04 22:36:19 +0100

    component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Categories from "./components/Categories";
 import Products from "./components/Products";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Deals from "./components/Deals";
-import Blog from "./components/Blog"
+import Blog from "./components/Blog";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
-      {/*  <Categories /> */}
       <Deals />
       <Products />
-     {/*  <About /> */}
-     <Blog />
+      <Blog />
       <Contact />
     </div>
   );
